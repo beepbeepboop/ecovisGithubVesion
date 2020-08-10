a053e63fabd8c33ca6d3e898b086a7a43d3c3f0d
Shahil2011 <shahilpoonsamy@gmail.com>
2020-08-10 17:25:30 +0200

    Initial commit, created main file which does nothing

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Can you guys see this? This post was made by [Shahil Gang]");
+    }
+}
