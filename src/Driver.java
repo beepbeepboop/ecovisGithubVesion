ff335c5923f5d9726863ef86770de1832741a9cc
Jordan Levieux <LVXJOR001@myuct.ac.za>
2020-09-07 15:53:39 +0200

    Refactored the data structures for plant data

--- src/Driver.java
+++ src/Driver.java
@@ -1,72 +1,78 @@
-import sun.awt.image.ImageWatched;
+//import sun.awt.image.ImageWatched;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Vector;
+
+import javax.lang.model.util.ElementScanner6;
+
 import java.util.Locale;
 
 public class Driver
 {
-    private static ElevationGrid elevationGrid; //Can't initialise just yet
-    private static HashMap<Integer, Species> undergrowthSpecies = new HashMap<Integer,Species>(); //Links species by ID
-    private static HashMap<Integer, Species> canopySpecies = new HashMap<Integer,Species>(); //Links species by ID
-    private static LinkedList<Plant> undergrowthPlants = new LinkedList<Plant>();   //Contains a list of all Plants - order and indexing is irrelevant at current stage of code
-    private static LinkedList<Plant> canopyPlants = new LinkedList<Plant>();   //Contains a list of all Plants - order and indexing is irrelevant at current stage of code
+    //Store Terrain Data in an ElevationGrid & Plant Data in Vectors
+    private static ElevationGrid elevationGrid; //Will basically just need this data to produce a background image, then it can be yeeted
+    private static Vector<Species> species;
+    private static Vector<Plant> undergrowthPlants;
+    private static Vector<Plant> canopyPlants;
 
     public static void main(String[] args) 
     {
         // Class details:
         // Plant: int speciesID, float x, y, z, height, canopyRadius
         // ElevationGrid: int dimX, dimY, float gridSpacing, latitude, float[][] grid
-        // Species: int speciesID, float minHeight, maxHeight, heightRatio, int numPlants
-        // Species: int speciesID, string commonName,latinName
+        // Species: int speciesID, String commonName, String latinName
 
         //Get array of files
-        String path = "./data/";
+        String path = "../data/";
         File[] fileArray = findFiles(path);
         for(File file : fileArray)
         {
-            String fileName = file.getPath();
+            String fileName = file.getName();
             System.out.println("Reading file: " + fileName);
+
             if(fileName.contains(".elv"))
-                readElv(fileName);
+                elevationGrid = readElv(file);
             else if(fileName.contains(".pdb"))
-                readPdb(fileName);
+            {
+                if(fileName.contains("undergrowth"))
+                    undergrowthPlants = readPdb(file);
+                else
+                    canopyPlants = readPdb(file);
+            }
             else if(fileName.contains(".spc"))
-                readSpc(fileName);
+                species = readSpc(file);
             else
             {
-                System.out.println("Error: Unrecognisable file type");
+                System.out.println("Error: "+fileName+" is an unrecognised file type.");
                 System.exit(1);
             }
         }
 
-        System.out.println(canopySpecies);
-        System.out.println(undergrowthSpecies);
+        //System.out.println(canopySpecies);
+        //System.out.println(undergrowthSpecies);
         //System.out.println(canopyPlants);
         //System.out.println(undergrowthPlants);
         //System.out.println(elevationGrid);
     }
 
-    private static void readSpc(String fileName)
+    private static Vector<Species> readSpc(File file)
     {
+		Vector<Species> spc = new Vector<Species>();
         try
         {
-            Scanner f = new Scanner(new File((fileName)));
+            Scanner f = new Scanner(file);
             while(f.hasNextLine())
             {
-                Scanner line = new Scanner(f.nextLine());
+                Scanner line = new Scanner(f.nextLine()).useLocale(Locale.US);
                 line.useDelimiter(" “");
                 int speciesId = line.nextInt();
                 String commonName = line.next().replace("”", "");
                 String latinName = line.next().replace("”", "");
-
-                if(canopySpecies.containsKey(speciesId))
-                    canopySpecies.get(speciesId).setName(commonName,latinName);
-                if(undergrowthSpecies.containsKey(speciesId))
-                    undergrowthSpecies.get(speciesId).setName(commonName,latinName);
+				spc.add(new Species(speciesId, commonName, latinName));
             }
             f.close();
         }
@@ -74,96 +80,93 @@ public class Driver
         {
             System.out.println(e);
         }
+
+		spc.trimToSize();
+		return spc;
     }
 
-    private static void readPdb(String fileName)
+    private static Vector<Plant> readPdb(File file)
     {
+        Vector<Plant> ret = new Vector<Plant>();
+
         try
-        {
-            Scanner f = new Scanner(new File((fileName)));
-            Scanner line = new Scanner(f.nextLine());
+        {    
+            Scanner f = new Scanner(file);
+            Scanner line = new Scanner(f.nextLine()).useLocale(Locale.US);
             int numSpecies = line.nextInt();
             int speciesId, numPlants;
-            float minHeight, maxHeight, heightRatio;
+            //float minHeight, maxHeight, heightRatio;
             for(int i = 0; i < numSpecies; i++)
             {
-                line = new Scanner(f.nextLine());
+                line = new Scanner(f.nextLine()).useLocale(Locale.US);
                 speciesId = line.nextInt();
-                minHeight = line.nextFloat();
+                /*minHeight = line.nextFloat();
                 maxHeight = line.nextFloat();
-                heightRatio = line.nextFloat();
-                line = new Scanner(f.nextLine());
+                heightRatio = line.nextFloat();*/
+                line = new Scanner(f.nextLine()).useLocale(Locale.US);
                 numPlants = line.nextInt();
-
-                // Insert species data into hashMap - check for undergrowth or canopy
-                if(fileName.contains("canopy"))
-                    undergrowthSpecies.put(speciesId,new Species(speciesId,minHeight,maxHeight,heightRatio,numPlants));
-                else //Assumption that files will be named appropriately - all .pdb files will either be named canopy or undergrowth
-                    canopySpecies.put(speciesId,new Species(speciesId,minHeight,maxHeight,heightRatio,numPlants));
 
                 for(int j = 0; j < numPlants; j++)
                 {
-                    line = new Scanner(f.nextLine());
+                    line = new Scanner(f.nextLine()).useLocale(Locale.US);
                     float xPos = line.nextFloat();
                     float yPos = line.nextFloat();
                     float zPos = line.nextFloat();
                     float height = line.nextFloat();
                     float radius = line.nextFloat();
-
-                    //Insert plant data into list
-                    //Possible ordering involves Min/Max heap of plants ordered by their radial distance from origin
-                    if(fileName.contains("undergrowth"))
-                        undergrowthPlants.add(new Plant(speciesId,xPos,yPos,zPos,height,radius));
-                    else
-                        canopyPlants.add(new Plant(speciesId,xPos,yPos,zPos,height,radius));
+                    ret.add(new Plant(speciesId, xPos, yPos, zPos, height, radius));
                 }
             }
-
-            line.close();
+			line.close();
             f.close();
         }
         catch (FileNotFoundException e)
         {
             System.out.println(e);
         }
+		ret.trimToSize();
+		return ret;
     }
 
-    private static void readElv(String fileName)
+    private static ElevationGrid readElv(File file)
     {
         try
         {
-            Scanner f = new Scanner(new File((fileName)));
-            Scanner line = new Scanner(f.nextLine());
+			ElevationGrid elevGrid;
+            Scanner f = new Scanner(file);
+            Scanner line = new Scanner(f.nextLine()).useLocale(Locale.US);
             int dimX = line.nextInt();
             int dimY = line.nextInt();
             float gridSpacing = line.nextFloat();
             float latitude = line.nextFloat();
-            elevationGrid = new ElevationGrid(dimX,dimY,gridSpacing,latitude);
+
+            elevGrid = new ElevationGrid(dimX,dimY,gridSpacing,latitude);
             float elevation;
             for(int x = 0; x < dimX; x++)
             {
-                line = new Scanner(f.nextLine());
+                line = new Scanner(f.nextLine()).useLocale(Locale.US);
                 for(int y = 0; y < dimY; y++)
                 {
                     elevation = line.nextFloat();
-                    elevationGrid.setElevation(x,y,elevation);
+                    elevGrid.setElevation(x,y,elevation);
                 }
             }
-            line.close();
+			line.close();
             f.close();
+			return elevGrid;
         }
         catch (FileNotFoundException e)
         {
             System.out.println(e);
         }
+		return null;
+		
     }
 
     //Returns list of files in a directory
     private static File[] findFiles(String path)
     {
-        //TODO: Order files so .spc is last read. Temporary fix: rename .spc file so it appears last when directory is ordered alphabetical
         File f = new File(path);
         return f.listFiles();
-        //return new String[]{"./data/S2000-2000-512.elv", "./data/S2000-2000-512_canopy.pdb", "./data/S2000-2000-512_undergrowth.pdb", "./data/S2000-2000-512.spc"}; //Temporary List
     }
 }
--- src/Species.java
+++ src/Species.java
@@ -1,35 +1,25 @@
 public class Species
 {
     //Class Variables
-    private int speciesID, numPlants;
-    private float minHeight, maxHeight, heightRatio;
+    private int speciesID;
     private String commonName, latinName;
 
     //Constructors
-    public Species(int id, float min, float max, float hRatio, int num)
+    public Species(int id, String cName, String lName)
     {
         speciesID = id;
-        minHeight = min;
-        maxHeight = max;
-        heightRatio = hRatio;
-        numPlants = num;
+        commonName = cName;
+        latinName = lName;
     }
 
     //Getters and Setters
     public int getSpeciesID(){return speciesID;}
-    public int getNumPlants(){return numPlants;}
-    public float getMinHeight(){return minHeight;}
-    public float getMaxHeight(){return maxHeight;}
-    public float getHeightRatio(){return heightRatio;}
     public String getCommonName(){return commonName;}
     public String getLatinName(){return latinName;}
 
     public void setSpeciesID(int id){speciesID = id;}
-    public void setNumPlants(int num){numPlants = num;}
-    public void setMinHeight(int min){minHeight = min;}
-    public void setMaxHeight(int max){maxHeight = max;}
-    public void setHeightRatio(int hRatio){heightRatio = hRatio;}
-    public void setName(String com, String lat){commonName = com; latinName = lat;}
+    public void setCommonName(String com){commonName = com;}
+    public void setLatinName(String lat){latinName = lat;}
 
 
     //To String for debugging
@@ -38,9 +28,6 @@ public class Species
         return new String(
         "Species Info:"+
         "\nCommon Name: "+commonName+
-        "\nLatin Name: "+latinName+
-        "\nNumber of plants in this species: "+numPlants+
-        "\nMin & Max Height: "+minHeight+" Min, "+maxHeight+" Max"+
-        "\nCanopy to Height Ratio: "+heightRatio+"\n");
+        "\nLatin Name: "+latinName+"\n");
     }
 }
