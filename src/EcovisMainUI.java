3475e14a3ba8c84eae4453c0af123ed4fe3c42b4
Declan <iamsuperboy01@gmail.com>
2020-09-16 05:29:47 +0200

    Messed around with some basic labels,stages and scenes as well as a parent VBox for the UI

--- src/EcovisMainUI.java
+++ src/EcovisMainUI.java
@@ -2,6 +2,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class EcovisMainUI extends Application {
@@ -20,7 +23,20 @@ public class EcovisMainUI extends Application {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         //primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        VBox parent1 = new VBox();
+        Label label1 = new Label("Some label text"); //child node for parent1
+        Label label2 = new Label("Some additional label text"); //child node for parent1
+        parent1.getChildren().addAll(label1,label2);
+
+        Scene firstScene = new Scene(parent1);
+        primaryStage.setHeight(500);
+        primaryStage.setWidth(600);
+        primaryStage.setScene(firstScene);primaryStage.show();
+
+
+        Stage filterwindow = new Stage();
+        filterwindow.initModality(Modality.NONE);
+
     }
 
     @Override
